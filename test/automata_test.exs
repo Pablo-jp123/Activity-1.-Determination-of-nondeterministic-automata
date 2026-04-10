93c93682bbe1d49bb7cc67184de225004e6c1764
Pablo-jp123 <A01739415@tec.mx>
2026-04-09 18:34:59 -0600

    Pregunta 2 final funcionando

--- test/automata_test.exs
+++ test/automata_test.exs
@@ -1,8 +1,19 @@
 defmodule AutomataTest do
   use ExUnit.Case
-  doctest Automata
 
-  test "greets the world" do
-    assert Automata.hello() == :world
+  test "determinize works" do
+    nfa = %{
+      states: [:q0, :q1],
+      alphabet: [:a],
+      transitions: %{
+        {:q0, :a} => [:q0, :q1]
+      },
+      start: :q0,
+      accept: [:q1]
+    }
+
+    dfa = Automata.determinize(nfa)
+
+    assert dfa != nil
   end
 end
