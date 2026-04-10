99bbcaf18a6a6fd975dee357349a7aebeb8b82a5
Pablo-jp123 <A01739415@tec.mx>
2026-04-09 18:30:18 -0600

    Pregunta 2 corregida

--- lib/automata.ex
+++ lib/automata.ex
@@ -1,18 +1,48 @@
 defmodule Automata do
-  @moduledoc """
-  Documentation for `Automata`.
-  """
+  def determinize(nfa) do
+    start = MapSet.new([nfa.start])
 
-  @doc """
-  Hello world.
+    build_dfa([start], MapSet.new([start]), %{}, nfa, [])
+  end
 
-  ## Examples
+  defp build_dfa([], visited, transitions, nfa, finals) do
+    %{
+      states: MapSet.to_list(visited),
+      alphabet: nfa.alphabet,
+      transitions: transitions,
+      start: MapSet.new([nfa.start]),
+      accept: finals
+    }
+  end
 
-      iex> Automata.hello()
-      :world
+  defp build_dfa([current | rest], visited, transitions, nfa, finals) do
+    {new_transitions, new_states} =
+      Enum.reduce(nfa.alphabet, {transitions, []}, fn symbol, {trans_acc, states_acc} ->
+        next =
+          current
+          |> Enum.flat_map(fn state ->
+            Map.get(nfa.transitions, {state, symbol}, [])
+          end)
+          |> MapSet.new()
 
-  """
-  def hello do
-    :world
+        trans_acc = Map.put(trans_acc, {current, symbol}, next)
+
+        if MapSet.member?(visited, next) or MapSet.size(next) == 0 do
+          {trans_acc, states_acc}
+        else
+          {trans_acc, [next | states_acc]}
+        end
+      end)
+
+    visited = Enum.reduce(new_states, visited, fn s, acc -> MapSet.put(acc, s) end)
+
+    finals =
+      if Enum.any?(current, fn s -> s in nfa.accept end) do
+        [current | finals]
+      else
+        finals
+      end
+
+    build_dfa(rest ++ new_states, visited, new_transitions, nfa, finals)
   end
 end
